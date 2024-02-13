2c2dcf7b1329a7155fe7d3ec5fe210ff884d0a14
Aysllan Ferreira <aysllan.ferreiraks@gmail.com>
2024-02-13 12:38:23 -0300

    chore: Updating files for Django

--- main.tf
+++ main.tf
@@ -17,12 +17,6 @@ resource "aws_instance" "app_server" {
   ami           = "ami-0c7217cdde317cfec"
   instance_type = "t2.micro"
   key_name = "iac-terraform"
-  user_data = <<-EOF
-              #!/bin/bash
-              cd /home/ubuntu
-              echo "<h1>Hello, World</h1>" > index.html
-              nohup busybox httpd -f -p 8080 &
-              EOF
 
   tags = {
     Name = "First Terraform"
